176d4d4eab13b7e73bf79cdba4c80147659a2e2d
michaeldymock25 <60867473+michaeldymock25@users.noreply.github.com>
2021-11-10 14:52:46 +0800

    Update functions.R

--- R/functions.R
+++ R/functions.R
@@ -61,41 +61,20 @@ clm <- function(data, T_follow, T_int, prior_sd, plot_it = TRUE, chains = 8, war
             refresh = 0,
             iter_warmup = warmup_iter, 
             iter_sampling = sampling_iter)
-  beta_draws <- fit$draws("beta")
-  tab_beta <- t(apply(beta_draws, 3, summ_fun))
-  names(dimnames(tab_beta)) <- c("beta[time,arm]", "statistic")
-  pis_draws <- plogis(apply(beta_draws, 3, function(x) x))
-  pis <- sapply(1:length(levels(data$arm)), function(arm){
-            pis <- pis_draws[,matrix(1:ncol(pis_draws), ncol = length(levels(data$arm)))[,arm]]
-            pis_temp <- cbind(1, 1, 1 - pis)
-            rowSums(sapply(1:ncol(pis), function(i) pis[,i]*rowProds(pis_temp[,1:(i+1)])))})
-  tab_pi <- t(apply(pis, 2, summ_fun))
-  dimnames(tab_pi)[[1]] <- levels(data$arm)
-  names(dimnames(tab_pi)) <- c("Arm", "statistic")
   
-  dat_vis_all <- data.frame(Arm = fct_inorder(rep(paste("Arm", levels(data$arm)), each = length(pis_draws)/length(levels(data$arm)))),
-                            x = as.vector(pis_draws),
-                            Time = fct_inorder(rep(rep(paste(T_follow[-length(T_follow)], "-", T_follow[-1]), each = nrow(pis_draws)),
-                                                   length(levels(data$arm)))))
-  p_all <- ggplot(dat_vis_all, aes(x = x)) + 
-              facet_wrap(~Arm) +
-              geom_freqpoly(bins = 25, aes(y = stat(count / sum(count)),  colour = Time), size = 1) +
-              scale_colour_manual("Time Period", values = c("#000000", "#E69F00", "#56B4E9", "#009E73",
-                                                            "#F0E442", "#0072B2", "#D55E00", "#CC79A7")) +
-              xlab(expression(pi[paste(j,tau)])) +
-              ylab("Density")
-  dat_vis_arm <- data.frame(Arm = fct_inorder(rep(paste("Arm", levels(data$arm)), each = nrow(pis))), x = as.vector(pis))
-  p_arm <- ggplot(dat_vis_arm, aes(x = x)) + 
-                geom_freqpoly(bins = 25, aes(y = stat(count / sum(count)), colour = Arm), size = 1) +
-                scale_colour_manual("Arm", values = c("#000000", "#E69F00", "#56B4E9", "#009E73",
-                                                      "#F0E442", "#0072B2", "#D55E00", "#CC79A7")) +
-                xlab(expression(pi[j])) +
-                ylab("Density")
-  if(plot_it){
-    print(p_all)
-    print(p_arm)
-  }
-  return(list(tab_beta = tab_beta, tab_pi = tab_pi, p_all = p_all, p_arm = p_arm, pis = pis))
+  beta_draws <- data.table(apply(fit$draws("beta"), 3, function(x) x))
+  colnames(beta_draws) <- paste("beta", as.vector(outer(1:(length(T_follow)-1), 1:J, FUN = "paste", sep = "_")), sep = "_")
+  
+  pi_tau_draws <- beta_draws[, lapply(.SD, plogis)]
+  colnames(pi_tau_draws) <- paste("pi", as.vector(outer(1:(length(T_follow)-1), 1:J, FUN = "paste", sep = "_")), sep = "_")
+  
+  pi_draws <- data.table(sapply(1:J, function(arm){
+                          pis <- as.matrix(pi_tau_draws[,matrix(1:ncol(pi_tau_draws), ncol = J)[,arm], with = FALSE])
+                          pis_temp <- cbind(1, 1, 1 - pis)
+                          rowSums(sapply(1:ncol(pis), function(i) pis[,i]*rowProds(pis_temp[,1:(i+1)])))}))
+  colnames(pi_draws) <- paste("pi", 1:J, sep = "_")
+
+  return(list(beta_draws = beta_draws, pi_tau_draws = pi_tau_draws, pi_draws = pi_draws))
 }  
 
 discard <- function(data, T_follow, T_int, prior_sd, plot_it = TRUE, chains = 8, warmup_iter = 500, sampling_iter = 2000, n_cores = 1){
@@ -117,24 +96,17 @@ discard <- function(data, T_follow, T_int, prior_sd, plot_it = TRUE, chains = 8,
             refresh = 0,
             iter_warmup = warmup_iter, 
             iter_sampling = sampling_iter)
-  beta_draws <- fit$draws("beta")
-  tab_beta <- t(apply(beta_draws, 3, summ_fun))
-  dimnames(tab_beta)[[1]] <- levels(data$arm)
-  names(dimnames(tab_beta)) <- c("Arm", "statistic")
+  
+  
   pis <- plogis(apply(beta_draws, 3, function(x) x))
-  tab_pi <- t(apply(pis, 2, summ_fun))
-  dimnames(tab_pi)[[1]] <- levels(data$arm)
-  names(dimnames(tab_pi)) <- c("Arm", "statistic")
-    
-  dat_vis_arm <- data.frame(Arm = fct_inorder(rep(paste("Arm", levels(data$arm)), each = nrow(pis))), x = as.vector(pis))
-  p_arm <- ggplot(dat_vis_arm, aes(x = x)) + 
-                geom_freqpoly(bins = 25, aes(y = stat(count / sum(count)), colour = Arm), size = 1) +
-                scale_colour_manual("Arm", values = c("#000000", "#E69F00", "#56B4E9", "#009E73",
-                                                      "#F0E442", "#0072B2", "#D55E00", "#CC79A7")) +
-                xlab(expression(pi[j])) +
-                ylab("Density")
-  if(plot_it) print(p_arm)
-  return(list(tab_beta = tab_beta, tab_pi = tab_pi, p_arm = p_arm, pis = pis))
+  
+  beta_draws <- data.table(apply(fit$draws("beta"), 3, function(x) x))
+  colnames(beta_draws) <- paste("beta", 1:J, sep = "_")
+  
+  pi_draws <- beta_draws[, lapply(.SD, plogis)]
+  colnames(pi_draws) <- paste("pi", 1:J, sep = "_")
+  
+  return(list(beta_draws = beta_draws, pi_draws = pi_draws))
 }
 
 transition <- function(data, T_follow, T_int, prior_sd, plot_it = TRUE, chains = 8, rep_data_sets = 10, warmup_iter = 500, sampling_iter = 2000, n_cores = 1){
@@ -166,36 +138,67 @@ transition <- function(data, T_follow, T_int, prior_sd, plot_it = TRUE, chains =
   y_inc_sum_draws <- apply(y_inc_sum_draws, 3, function(x) x)
   temp_data <- data[t_enrol + T_follow[length(T_follow)] < T_int]
   discard_mod <- cmdstan_model("discard.stan")
-  pis <- rbindlist(lapply(1:nrow(y_inc_sum_draws), function(i){
-              mod_data <- list(J = J,
-                               n = as.vector(table(temp_data$arm) + rowSums(n_inc)),
-                               y = unlist(temp_data[levels(arm), sum(y), by = .EACHI][,-"arm"]) + y_inc_sum_draws[i,],
-                               prior_sd = prior_sd)
-              fit <- discard_mod$sample(
-                        data = mod_data,
-                        chains = chains,
-                        parallel_chains = min(chains, n_cores),
-                        refresh = 0,
-                        iter_warmup = warmup_iter, 
-                        iter_sampling = sampling_iter)
-              beta_draws <- fit$draws("beta")
-              pis <- plogis(apply(beta_draws, 3, function(x) x))
-              data.table(pis)}))
-  beta_draws <- qlogis(as.matrix(pis))
-  tab_beta <- t(apply(beta_draws, 2, summ_fun))
-  dimnames(tab_beta)[[1]] <- levels(data$arm)
-  names(dimnames(tab_beta)) <- c("Arm", "statistic")
-  tab_pi <- t(apply(pis, 2, summ_fun))
-  dimnames(tab_pi)[[1]] <- levels(data$arm)
-  names(dimnames(tab_pi)) <- c("Arm", "statistic")
-    
-  dat_vis_arm <- data.frame(Arm = fct_inorder(rep(paste("Arm", levels(data$arm)), each = nrow(pis))), x = unlist(pis))
-  p_arm <- ggplot(dat_vis_arm, aes(x = x)) + 
+  beta_draws <- rbindlist(lapply(1:nrow(y_inc_sum_draws), function(i){
+                            mod_data <- list(J = J,
+                                             n = as.vector(table(temp_data$arm) + rowSums(n_inc)),
+                                             y = unlist(temp_data[levels(arm), sum(y), by = .EACHI][,-"arm"]) + y_inc_sum_draws[i,],
+                                             prior_sd = prior_sd)
+                            fit <- discard_mod$sample(
+                                                      data = mod_data,
+                                                      chains = chains,
+                                                      parallel_chains = min(chains, n_cores),
+                                                      refresh = 0,
+                                                      iter_warmup = warmup_iter, 
+                                                      iter_sampling = sampling_iter)
+                            beta_draws <- data.table(apply(fit$draws("beta"), 3, function(x) x))}))
+  colnames(beta_draws) <- paste("beta", 1:J, sep = "_")
+  
+  pi_draws <- beta_draws[, lapply(.SD, plogis)]
+  colnames(pi_draws) <- paste("pi", 1:J, sep = "_")
+  
+  return(list(beta_draws = beta_draws, pi_draws = pi_draws))
+}
+
+summarise <- function(draws, clm = TRUE){
+  tab_pi <- t(apply(draws$pi_draws, 2, summ_fun))
+  names(dimnames(tab_pi)) <- c("beta_time_arm", "statistic")
+  tab_beta <- t(apply(draws$beta_draws, 2, summ_fun))
+  names(dimnames(tab_beta)) <- c("beta_time_arm", "statistic")
+  if(clm){
+    tab_pi_tau <- t(apply(draws$pi_tau_draws, 2, summ_fun))
+    names(dimnames(tab_pi_tau)) <- c("Arm", "statistic")
+    return(list(tab_pi = tab_pi, tab_pi_tau = tab_pi_tau, tab_beta = tab_beta))
+  } else {return(list(tab_pi = tab_pi, tab_beta = tab_beta))}
+}
+
+plot_pis <- function(draws, type = "arm"){
+  if(type == "arm"){
+    dat_vis_arm <- data.frame(Arm = fct_inorder(rep(paste("Arm", 1:ncol(draws$pi_draws)), each = nrow(draws$pi_draws))),
+                              x = unlist(pi_draws))
+    p_arm <- ggplot(dat_vis_arm, aes(x = x)) + 
                 geom_freqpoly(bins = 25, aes(y = stat(count / sum(count)), colour = Arm), size = 1) +
                 scale_colour_manual("Arm", values = c("#000000", "#E69F00", "#56B4E9", "#009E73",
-                                                      "#F0E442", "#0072B2", "#D55E00", "#CC79A7")) +
+                                                      "#F0E442", "#0072B2", "#D55E00", "#CC79A7"), drop = TRUE) +
                 xlab(expression(pi[j])) +
                 ylab("Density")
-  if(plot_it) print(p_arm)
-  return(list(tab_beta = tab_beta, tab_pi = tab_pi, p_arm = p_arm, pis = pis))
+    return(p_arm)
+  } else if(type == "all"){
+    if(!("pi_tau_draws" %in% names(draws))) stop("this plot is only available for CLM output")
+    dat_vis_all <- data.frame(Arm = fct_inorder(rep(paste("Arm", 1:ncol(draws$pi_draws)), 
+                                                    each = nrow(draws$pi_tau_draws)*ncol(draws$pi_tau_draws)/ncol(draws$pi_draws))),
+                              x = unlist(draws$pi_tau_draws),
+                              Time = fct_inorder(rep(rep(paste(T_follow[-length(T_follow)], "-", T_follow[-1]), 
+                                                         each = nrow(draws$pi_tau_draws)), 
+                                                     ncol(draws$pi_draws))))
+    p_all <- ggplot(dat_vis_all, aes(x = x)) + 
+      facet_wrap(~Arm) +
+      geom_freqpoly(bins = 25, aes(y = stat(count / sum(count)),  colour = Time), size = 1) +
+      scale_colour_manual("Time Period", values = c("#000000", "#E69F00", "#56B4E9", "#009E73",
+                                                    "#F0E442", "#0072B2", "#D55E00", "#CC79A7")) +
+      xlab(expression(pi[paste(j,tau)])) +
+      ylab("Density")
+    return(p_all)
+  } else {
+    stop("type must be either 'arm' or 'all'")
+  }
 }
